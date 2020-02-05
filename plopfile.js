15630f62cfdb938dd397170d01e7a80ca31b7a3b
Tomek <tomek.buszewski@gmail.com>
2020-02-05 13:37:21 +0100

    feat: plop added (#3)

---
+++ plopfile.js
@@ -0,0 +1,51 @@
+const newFileInfo = {
+  userName: require("os").userInfo().username,
+  created: new Date()
+    .toISOString()
+    .replace(/T/, " ")
+    .replace(/\..+/, ""),
+};
+
+const checkForFile = filepath => require("fs").existsSync(filepath);
+
+module.exports = plop => {
+  plop.setGenerator("ui", {
+    description: "Create a component",
+    prompts: [
+      {
+        type: "input",
+        name: "name",
+        message: "What is your component's name?",
+      },
+      {
+        type: "list",
+        name: "type",
+        message: "What kind of component?",
+        choices: [
+          { name: "atom", value: "Atoms" },
+          { name: "molecule", value: "Molecules" },
+          { name: "organism", value: "Organisms" },
+        ],
+      },
+    ],
+    actions: [
+      {
+        type: "add",
+        path:
+          "./src/ui/{{pascalCase type}}/{{pascalCase name}}/{{pascalCase name}}.tsx",
+        templateFile: "./config/plop-templates/Component.tsx.hbs",
+        data: newFileInfo,
+      },
+      {
+        type: "add",
+        path: "./src/ui/{{pascalCase type}}/{{pascalCase name}}/index.ts",
+        templateFile: "./config/plop-templates/index.ts.hbs",
+      },
+      {
+        type: "append",
+        path: "./src/ui/{{pascalCase type}}/index.ts",
+        template: 'export * from "./{{pascalCase name}}";',
+      },
+    ],
+  });
+};
